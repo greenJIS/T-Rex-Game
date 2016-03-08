e48176de215acaf9500694145814885ed5edac0b
Victor Bolinches <vicboma1@users.noreply.github.com>
2016-03-08 13:56:18 +0100

    add folder refactors

--- Unity/src/main/Main/MVCAS/Adapter/IAdapter.cs
+++ Unity/src/main/Main/MVCAS/Adapter/IAdapter.cs
@@ -6,14 +6,11 @@ public interface IAdapter {
 	event EventHandler UpdateHandler;
 	event EventHandler FixedUpdateHandler;
 	event EventHandler Initialize;
-	bool IsInitialize{ get; set;}
-
+	bool IsInitialize { get; set;}
 	void OnLoadCoroutine ();
-
 	void StopCoroutine(IEnumerator coroutine);
 	Coroutine StartCoroutine (IEnumerator coroutine);
 	//T Instantiate<T> (T obj, UnityEngine.Vector3 vector3, UnityEngine.Quaternion quaternion) where T : UnityEngine.Object;
-
 	T GetComponent<T>();
 	T[] GetComponentsInChildren<T>();
 	T GetComponentInChildren<T>();
